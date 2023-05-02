66333d0f4dea73f2c65909b31e3802ac5389b19a
earthtennison <earth.tennison@gmail.com>
2023-05-02 23:55:51 +0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ngrok http 4000 --log=stdout > ngrok.log &
-python3 conversation.py &
-python3 app.py &
+python3 conversation.py >> app.log 2>&1 &
+waitress-serve --host 127.0.0.1 --port 4000 app:app >> app.log 2>&1 &
 python3 scheduler.py &
